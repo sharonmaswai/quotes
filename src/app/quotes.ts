76d52f7da74a9adc636369127fd478660e71e693
sharonmaswai <chepmaswai@gmail.com>
2019-04-30 10:51:19 +0300

    add properties to obto object

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,10 @@
 export class Quotes {
+    public showDescription:boolean;
+        constructor(public id:number,public upVote: number,
+            public downVote:number,public description:string,public name:string,public completeDate:Date, public upvote:number,public downvote:number){
+          this.showDescription=false;
+        }
+    
+      
     
 }
