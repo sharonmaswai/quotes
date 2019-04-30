cc5f159ef70f7b5e11707bb130d81f2f50a676e8
sharonmaswai <chepmaswai@gmail.com>
2019-04-30 09:28:10 +0300

    add new component quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesFormComponent
+    QuotesFormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
