8985ac7aa00c113d6fafac47e39d42d17496e5d1
sharonmaswai <chepmaswai@gmail.com>
2019-05-01 09:19:51 +0300

    add new property to constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
     public showDescription:boolean;
         constructor(public id:number,public upVote: number,
-            public downVote:number,public name:string,public description:string,){
+            public downVote:number,public name:string,public description:string,public submitter:string){
           this.showDescription=false;
         }
     
